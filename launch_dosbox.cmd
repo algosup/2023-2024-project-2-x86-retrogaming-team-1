bcaeb1989c8c218b5a8a36b4e55b0394b6ed567a
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-11-27 16:01:36 +0100

    Update launch_dosbox.cmd

--- launch_dosbox.cmd
+++ launch_dosbox.cmd
@@ -1,8 +1,30 @@
+@REM Launch dosbox 
+
+@REM @echo off is used to hide the commands from the user
+
 @echo off
+
+@REM Set the path to the directory where the script is located
+
 set "ROOT_DIR=%~dp0"
+
+@REM Set the path to the directory where the main.com file will be placed
 set "BIN_DIR=%ROOT_DIR%DosBox_exe"
+
+@REM Set the path to the dosbox executable
+
 set "DOSBOX_BIN=%ProgramFiles(x86)%\DOSBox-0.74-3\DOSBox.exe"
+
+@REM Set the path to the dosbox configuration file
 set "CONFIG_LOC=%ROOT_DIR%"
 
+
+@REM Execute the dosbox command to go to the DosBox_exe folder
+
 ::"%DOSBOX_BIN%" -conf "%CONFIG_LOC%"
+
+@REM Mount the DosBox_exe folder as the C drive
+@REM Go to the C drive
+@REM Clear the screen
+
 "%DOSBOX_BIN%" -c "MOUNT c %BIN_DIR%" -c "c:" -c "cls"
