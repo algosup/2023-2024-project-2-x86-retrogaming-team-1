a1766c42df4bca2c8633ad7ead1df80586b93c23
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-11-27 15:56:31 +0100

    Update build.cmd

--- src/build.cmd
+++ src/build.cmd
@@ -1,23 +1,23 @@
-REM This script is used to build the main.com file
-REM  @echo off is used to hide the commands from the user
+@REM This script is used to build the main.com file
+@REM  @echo off is used to hide the commands from the user
 @echo off
 
-REM Set the path to the directory where the script is located
+@REM Set the path to the directory where the script is located
 set "SCRIPT_DIR=%~dp0"
 
-REM Set the root directory to the parent directory of the script directory
+@REM Set the root directory to the parent directory of the script directory
 set "ROOT_DIR=%SCRIPT_DIR%\..\"
 
-REM Set the path to the nasm executable
+@REM Set the path to the nasm executable
 set "NASM=%ROOT_DIR%.\nasm\nasm"
 
-REM Set the path to the directory where the main.com file will be placed
+@REM Set the path to the directory where the main.com file will be placed
 set "BIN_DIR=%ROOT_DIR%\DosBox_exe"
 
-REM Execute the nasm command to build the main.com file
+@REM Execute the nasm command to build the main.com file
 "%NASM%" main.asm -f bin -o "%BIN_DIR%\main.com"
 
-REM Pause the script
+@REM Pause the script
 pause
 
 
