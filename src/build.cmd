87c5ded38574df0eae4c570d5e1e732723888472
Salaheddine Namir <71770514+T3rryc@users.noreply.github.com>
2023-12-04 16:23:59 +0100

    Update build.cmd

--- src/build.cmd
+++ src/build.cmd
@@ -17,8 +17,23 @@ set "NASM=%ROOT_DIR%.\nasm\nasm"
 @REM Set the path to the directory where the main.com file will be placed
 set "BIN_DIR=%ROOT_DIR%\DosBox_exe"
 
+@REM Set the path to the dosbox configuration file
+set "CONFIG_LOC=%ROOT_DIR%"
+
 @REM Execute the nasm command to build the main.com file
 "%NASM%" main.asm -f bin -o "%BIN_DIR%\main.com"
+
+set "DOSBOX_BIN=%ProgramFiles(x86)%\DOSBox-0.74-3\DOSBox.exe"
+
+@REM Execute the dosbox command to go to the DosBox_exe folder
+
+::"%DOSBOX_BIN%" -conf "%CONFIG_LOC%"
+
+@REM Mount the DosBox_exe folder as the C drive
+@REM Go to the C drive
+@REM Clear the screen
+
+"%DOSBOX_BIN%" -c "MOUNT c %BIN_DIR%" -c "c:" -c "cls" -c main
 
 @REM Pause the script
 pause
