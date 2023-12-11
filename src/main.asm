576e915c011003b88f8be51fa5ff6231f202f800
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2023-12-11 09:46:28 +0100

    create maps branch

--- src/main.asm
+++ src/main.asm
@@ -1,107 +1,131 @@
-; TODO
-; map
-; collisions
-org 100h    
+org 100h
 
-section .bss
+section .data
 
-    backBufferSeg resw 1
+windowWidth equ 256
+windowHeight equ 200
+mazeHeight equ 25
+nb_tile equ 0
+
+wall:   db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        db 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        db 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        db 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        db 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        db 0x00, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
+path:   db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00   
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+        db 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+
 
 section .text
 
 global _start
 
-    _start:
-    
-    call heapInit
-    mov ax, 1000h
-    call heapAllocParagraph
-    mov [backBufferSeg], ax
-
-    mov ah, 00h                 ; set video mode requirement
-    mov al, 013h                ; set Video Mode 4F02h (VBE mode 101h) - 640x480, 256 colors
+_start:
+    mov ah, 00h
+    mov al, 013h
     int 10h
 
-    mov di, 0                   ; set the original coordinate of the sprite
-
-    ; mov ax, 0xA000
-    ; mov es, ax
-    ; call drawMaze
-    mov si, pacmanR_00          ; select the sprite to be displayed
-    call drawPacman             ; display the selected sprite
-
-    mainLoop:
-
-    mov bl, 0xFF                ; move into bl the color we want to clear with
-    call clearGhost             ; clear the previous ghost
-
-    mov di, [xPos]              ; set the original coordinate of the sprite    
-
-    call readKeyb               ; call the keyboard reader
-
-    ; This loop is to slow down the animation
-    mov cx, 5000                ; 50000 is the time we wait before moving the sprite
-    waitloop:                   ; cx is a loop register
-    loop waitloop               ; once count = 0 exit the loop
-    
-    jmp mainLoop                ; loop
-
-    ;dos box default video mode
-    mov ax, 03h                 ; set into video mode
-    int 21h                     ; call DOS interupt
-
-    int 20h                     ; quit 
-
-    ; procedures:
-
-    clearGhost:
-    mov di, [xPos]              ; input the position of the sprite
-    push bx                     
-    mov bx, 0xA000              ; set the video memory segment to 0xA000
-    mov es, bx
-    pop bx
-    mov dx, 16                  ; set the destination index to 1§
-    .eachLine:      
-        mov cx, 16              ; set the count register to 16 (number of pixel to copy per line)
-        rep movsb               ; repeat the move byte action (copying pixel)
-        add di, 320-16          ; move the destination index to the next line (320 pixel per line)
-        dec dx                  ; decrement the loop counter (dx)
-        jnz .eachLine           ; jump to .eachline if not zero
-    ret                         ; return to the caller
-
-
-    ; si must have the sprite address
-    ; di must have the target address
-    drawPacman:
-    mov ax, 0xA000              ; memory location of the video mode
+    mov ax, 0xA000
     mov es, ax
-    mov dx, 16                  ; set the destination index to 16 (starting position in video memory)
-    .eachLine:                  ; loop till each line of the sprite is printed
-        mov cx, 16              ; set the count register to 16 (number of pixel to copy per line)
-        rep movsb               ; repeat the move byte action (copying pixel)
-        add di, 320-16          ; move the destination index to the next line (320 pixel per line)
-        dec dx                  ; decrement the loop counter (dx) and jump to .eachLine if not zero
-        jnz .eachLine
-        ret                     ; return to the main loop
-    
-    presentBackBuffer:
-    push ds
-    push es
-    mov ax, [cs:backBufferSeg]
-    mov ds, ax
-    mov ax, 0A000h
+    call draw_maze
+
+draw_maze:
+
+    mov si, maze
+    mov cl, 0b1000_0000
+    mov al, [si]
+
+    mov di, windowWidth*mazeHeight
+    xor bx, bx
+
+    ; loop until the maze is drawn  
+    loopDraw: 
+        cmp byte [nb_tile], 8               
+        jae resetComp
+
+        cmp di, windowWidth*windowHeight-8
+        jae .exit
+
+; check wether or not a byte is set (1)
+        test al, cl                  
+        jnz .drawTile                       ; if set (1) means its a wall             
+        jmp .drawPath                       ; if not set (0) means its a path
+
+        .drawTile:
+            push si                         ; push si, cx and ax to reuse them later on
+            push cx
+            push ax
+            mov si, wall                    ; select the sprite of the wall
+            jmp createTile
+        .drawPath:
+            push si                         ; push si, cx and ax to reuse them later on
+            push cx
+            push ax
+            mov si, path                    ; select the sprite of the path 
+            jmp createTile
+        
+         .exit:
+        mov ah, 4ch
+        xor al, al
+        int 21h
+
+createTile:
+
+    call draw_tile                          ; draw the sprite 
+
+    pop ax                                  ; pop si, cx and ax /!\ the order
+    pop cx
+    pop si
+
+    shr cl, 1                               ; shift to the right by 1
+
+    sub di, windowWidth*8                   ; after drawing a tile, go back to the previous line 
+    add di, 8                               ; add the width of a tile
+    add bx, 8
+    add byte [nb_tile], 1                   ; increase the counter by one 
+    cmp bx, windowWidth                     ; compare to the window width 
+    jb loopDraw                             ; if not finished loop
+    add di, windowWidth*7                   ; go to the next row 
+    xor bx, bx
+    jmp loopDraw
+
+    cmp cl, 0                               ; if cl is equal to 0 reset si
+    je resetComp
+
+;reseting cl means the current byte is finished 
+resetComp:
+    mov byte [nb_tile], 0                   ; reset the counter        
+    mov cl, 0b1000_0000                     ; reset cl 
+    inc si                                  ; increase si to get to the next byte in maze
+    mov al, [si]                            ; move the current byte to al
+
+    jmp loopDraw
+
+;draw in 8x8
+draw_tile:
+    mov ax, 0xA000
     mov es, ax
-    xor si, si
-    xor di, di
-    mov cx, 320*200
-    rep movsb
-    pop es
-    pop ds
+    mov dx, 8
+        .eachLine:
+            mov cx, 8
+            rep movsb
+            add di, windowWidth-8
+            dec dx
+            jnz .eachLine
     ret
 
-    %include "sprites.inc"          ; include the file with the sprites
-    %include "movement.inc"         ; include the file for the movement
-    %include "heapLibrarie.inc"     ; include the heap librarie
-    ;%include "maze.inc"             ; include the map drawing
-    ;%include "maze_sprite.inc"
-    %include "keyboard_handler.inc"
+mov ax, 03h
+int 21h
+int 20h
+
+%include "maze_sprite.inc"
